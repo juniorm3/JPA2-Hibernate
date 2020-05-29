c838132379a435a460d47c3fe169ba86b0e84cb8
Mario <mario.martins@sonda.com>
2020-05-29 11:18:49 -0300

    Ciclo de vida e Estados dos objetos

--- locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Carro.java
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Carro.java
@@ -1,6 +1,7 @@
 package com.algaworks.curso.jpa2.modelo;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -15,6 +16,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @NamedQueries({
@@ -33,6 +38,9 @@ public class Carro {
 	private ModeloCarro modelo;
 	private List<Acessorio> acessorios;
 	private List<Aluguel> alugueis;
+	
+	private Date dataCriacao;
+	private Date dataModificacao;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -97,6 +105,33 @@ public class Carro {
 	}
 	public void setAlugueis(List<Aluguel> alugueis) {
 		this.alugueis = alugueis;
+	}
+	
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getDataModificacao() {
+		return dataModificacao;
+	}
+	public void setDataModificacao(Date dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+	
+	@PrePersist
+	@PreUpdate
+	public void congiuraDatasCriacaoAlteracao() {
+		this.dataModificacao = new Date();
+		
+		if(this.getDataCriacao() == null) {
+			this.dataCriacao = new Date();
+		}
 	}
 	
 	@Override
