f9e362d0f0831e750492a81c9d5b3688b9359042
Mario <mario.martins@sonda.com>
2020-06-08 11:02:55 -0300

    Resultados complexos, tuplas e construtores

--- locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemplosCriteria.java
+++ locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemplosCriteria.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -71,6 +72,56 @@ public class ExemplosCriteria {
 		System.out.println("Soma de todos os alugueis: " + total);
 	}
 	
+	@Test
+	public void resultadoComplexo() {
+		CriteriaBuilder builder  = manager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
+		
+		Root<Carro> carro = criteriaQuery.from(Carro.class);
+		criteriaQuery.multiselect(carro.get("placa"), carro.get("valorDiaria"));
+		
+		TypedQuery<Object[]> query = manager.createQuery(criteriaQuery);
+		List<Object[]> resultado = query.getResultList();
+		
+		for (Object[] valores : resultado) {
+			System.out.println(valores[0] + " - " + valores[1]);
+		}
+	}
+		
+	@Test
+	public void resultadoTupla() {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Tuple> criteriaQuery = builder.createTupleQuery();
+		
+		Root<Carro> carro = criteriaQuery.from(Carro.class);
+		criteriaQuery.multiselect(carro.get("placa").alias("placaCarro"),
+				carro.get("valorDiaria").alias("valorCarro"));
+		
+		TypedQuery<Tuple> query = manager.createQuery(criteriaQuery);
+		List<Tuple> resultado = query.getResultList();
+		
+		for (Tuple tupla : resultado) {
+			System.out.println(tupla.get("placaCarro") + " - " + tupla.get("valorCarro"));
+			
+		}
+	}
+	
+	@Test
+	public void resultadoConstrutores() {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<PrecoCarro> criteriaQuery = builder.createQuery(PrecoCarro.class);
+		
+		Root<Carro> carro  = criteriaQuery.from(Carro.class);
+		criteriaQuery.select(builder.construct(PrecoCarro.class, 
+					carro.get("placa"), carro.get("valorDiaria")));
+		
+		TypedQuery<PrecoCarro> query = manager.createQuery(criteriaQuery);
+		List<PrecoCarro> resultado = query.getResultList();
+		
+		for (PrecoCarro precoCarro : resultado) {
+			System.out.println(precoCarro.getPlaca() + " - "+ precoCarro.getValor());
+		}
+	}
 	
 	
 	
