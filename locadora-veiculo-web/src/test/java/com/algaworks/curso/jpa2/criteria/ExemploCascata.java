3293725ec50c3ea56ae4aead86e409e5932085f0
Mario <mario.martins@sonda.com>
2020-06-17 16:48:34 -0300

    Gravação em cascata

---
+++ locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemploCascata.java
@@ -0,0 +1,53 @@
+package com.algaworks.curso.jpa2.criteria;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.curso.jpa2.modelo.Carro;
+import com.algaworks.curso.jpa2.modelo.Categoria;
+import com.algaworks.curso.jpa2.modelo.ModeloCarro;
+
+public class ExemploCascata {
+
+	private static EntityManagerFactory factory;
+
+	private EntityManager manager;
+
+	@BeforeClass
+	public static void init() {
+		factory = Persistence.createEntityManagerFactory("locadoraVeiculoPU");
+	}
+
+	@Before
+	public void setUp() {
+		this.manager = factory.createEntityManager();
+	}
+
+	@After
+	public void tearDown() {
+		this.manager.close();
+	}
+	
+	@Test
+	public void exemploEntidadeTransiente() {
+		Carro carro = new Carro();
+		carro.setCor("Preto");
+		carro.setPlaca("AAA-1111");
+		
+		ModeloCarro modelo = new ModeloCarro();
+		modelo.setCategoria(Categoria.ESPORTIVO);
+		modelo.setDescricao("Ferrari");
+		carro.setModelo(modelo);
+		
+		this.manager.getTransaction().begin();
+		this.manager.persist(carro);
+		this.manager.getTransaction().commit();
+	}
+
+}
